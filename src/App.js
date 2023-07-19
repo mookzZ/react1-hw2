40736d294f577c02da9536848e14c0a2082d9d90
acided <homejet2007@gmail.com>
2023-07-19 12:08:37 +0600

    hw2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Square extends React.Component {
+    render() {
+        return (
+            <button
+                className="square"
+                style={{ backgroundColor: this.props.backgroundColor }}
+                onClick={this.props.onClick}
+            ></button>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            backgroundColorA: "red",
+            backgroundColorB: "blue",
+        };
+    }
+
+    handleClick = () => {
+        const newColorA = this.state.backgroundColorA === "red" ? "blue" : "red";
+        const newColorB = this.state.backgroundColorB === "red" ? "blue" : "red";
+        this.setState({
+            backgroundColorA: newColorA,
+            backgroundColorB: newColorB,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Square
+                    backgroundColor={this.state.backgroundColorA}
+                    onClick={this.handleClick}
+                />
+                <Square
+                    backgroundColor={this.state.backgroundColorB}
+                    onClick={this.handleClick}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
